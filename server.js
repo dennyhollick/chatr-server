1150133d142307f9f7b98920257d52d60e7a03d2
Denny Hollick <dennyhollick@gmail.com>
2017-07-27 13:39:36 -0700

    Added broadcast total users on connect

--- server.js
+++ server.js
@@ -31,8 +31,19 @@ wss.broadcast = function broadcast(data) {
 
 // Connect and run
 
+let connectedUsers = 0;
+
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+  connectedUsers += 1;
+
+  // Update total users across all clients upon connect
+  wss.broadcast({
+    type: 'systemStatus',
+    totalUsers: connectedUsers,
+  });
+  console.log(`Client connected, Total Users = ${connectedUsers}`);
+
+  // Receive Message or system event
 
   ws.onmessage = function (event) {
     const incomingMessage = JSON.parse(event.data);
@@ -45,6 +56,7 @@ wss.on('connection', (ws) => {
         const nameChangeBroadcast = {
           uuid: incomingMessage.uuid,
           content: `${incomingMessage.oldUsername} has changed their name to ${incomingMessage.newUserName}`,
+          type: 'nameChange',
         };
         wss.broadcast(nameChangeBroadcast);
         break;
@@ -68,5 +80,8 @@ wss.on('connection', (ws) => {
   };
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    connectedUsers -= 1;
+    console.log(`Client disconnected, Total Users = ${connectedUsers}`);
+  });
 });
